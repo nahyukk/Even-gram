24bbc7de49279dd3e55a998302e6f29dedd31d22
LJIEUN <eun773c@daum.net>
2024-12-10 00:14:10 +0900

    왼쪽 sidebar.js 다크모드 변환 부분 입니다.

--- js/sidebar.js
+++ js/sidebar.js
@@ -45,4 +45,46 @@ document.addEventListener('DOMContentLoaded', () => {
         event.stopPropagation();
         modeMenu.classList.remove('hidden');
     });
+
+    // 다크 모드 변환 시 바뀌는 부분
+    darkModeToggle.addEventListener('change', (event) => {
+        const isDarkMode = event.target.checked;
+        const icons = document.querySelectorAll('.logo-container img, .middle-container img, .bottom-container img, .mode-menu img');    //sidebar의 아이콘 변경
+        const menus = document.querySelectorAll('.dropdown-menu, .mode-menu');
+        const body = document.body;
+        const sidebar = document.querySelector('.left-container');
+        const hrEle = document.querySelectorAll('#moreMenu hr, #menu-header hr');
+
+        body.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff";   // body 적용 부분
+        body.style.color = isDarkMode ? "#ffffff" : "#000000";
+        sidebar.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff"; // 사이드바 부분 적용
+        sidebar.style.borderColor = isDarkMode ? "#383838" : "#dbdbdb";    // 사이드바 border 부분 적용
+
+        // 드롭다운 메뉴들 다크모드 변경
+        menus.forEach((menu) => {
+            menu.style.backgroundColor = isDarkMode ? "#272727" : "#ffffff";    //dropdown-menu 변경
+            menu.style.color = isDarkMode ? "#ffffff" : "#000000";
+        });
+
+        hrEle.forEach((hr) => {
+            hr.style.borderColor = isDarkMode ? "#444444" : "#dbdbdb4d";
+            hr.style.backgroundColor = isDarkMode ? "#444444" : "#dbdbdb4d";
+        });
+
+        if (isDarkMode) {
+            body.classList.add('dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+        }
+
+        // 아이콘 다크모드 변경
+        icons.forEach((icon) => {
+            const originSrc = icon.getAttribute('src');
+            if (isDarkMode) {
+                icon.setAttribute('src', originSrc.replace('icons/', 'icons/dark_'));
+            } else {
+                icon.setAttribute('src', originSrc.replace('icons/dark_', 'icons/'));
+            }
+        });
+    });
 });
