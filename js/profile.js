ad45f0816ce3ef31b2e561bb1e46d85b37c90f87
seongmin <min@seongminui-MacBookPro.local>
2024-12-11 00:12:50 +0900

    프로필 데이터 적용

--- js/profile.js
+++ js/profile.js
@@ -9,6 +9,23 @@ function fetchUser() {
 		})
 		.then((data) => {
 			console.log(data);
+			renderUser(data.user);
 		});
 }
+
+function renderUser(user) {
+	document.querySelector(".profile__image").src = randomImageURL;
+	document.querySelector(".profile__name").textContent = user.nickName;
+	document.querySelector("#profile__post-info-posts").textContent =
+		"게시물 " + user.posts.length;
+	document.querySelector("#profile__post-info-followers").textContent =
+		"팔로워 " + user.follower;
+	document.querySelector("#profile__post-info-following").textContent =
+		"팔로잉 " + user.following;
+	document.querySelector(".profile__description-title").textContent =
+		user.title;
+	document.querySelector(".profile__description-sub-title").textContent =
+		user.description;
+}
+
 fetchUser();
