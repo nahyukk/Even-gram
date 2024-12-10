a03eed57e587ec830559c2c7d358797558d5b887
seongmin <min@seongminui-MacBookPro.local>
2024-12-11 00:12:31 +0900

    json파일 호출

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,14 @@
-console.log("profile");
+
+function fetchUser() {
+	fetch("../json/profile.json")
+		.then((response) => {
+			if (!response.ok) {
+				console.log("error");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data);
+		});
+}
+fetchUser();
