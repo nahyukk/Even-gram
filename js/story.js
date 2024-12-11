b36d1114757f9bbaf1e2c30c87e68acdec9ff3b3
nahyun <knahyuk@gmail.com>
2024-12-12 01:03:16 +0900

    사진 당 4초 자동 재생 구현

--- js/story.js
+++ js/story.js
@@ -69,9 +69,12 @@ function togglePlayPause() {
   const storyPlayButton = document.getElementById("story-btn-play");
 
   if (isPlaying) {
-    storyPlayButton.innerHTML = `<svg aria-label="재생" class="x1lliihq x1n2onr6 xq3z1fi" fill="currentColor" height="16" role="img" viewBox="0 0 24 24" width="16"><title>재생</title><path d="M5.888 22.5a3.46 3.46 0 0 1-1.721-.46l-.003-.002a3.451 3.451 0 0 1-1.72-2.982V4.943a3.445 3.445 0 0 1 5.163-2.987l12.226 7.059a3.444 3.444 0 0 1-.001 5.967l-12.22 7.056a3.462 3.462 0 0 1-1.724.462Z"></path></svg>`;
-  } else {
     storyPlayButton.innerHTML = `<svg aria-label="일시 정지" class="x1lliihq x1n2onr6 xq3z1fi" fill="currentColor" height="16" role="img" viewBox="0 0 48 48" width="16"><title>일시 정지</title><path d="M15 1c-3.3 0-6 1.3-6 3v40c0 1.7 2.7 3 6 3s6-1.3 6-3V4c0-1.7-2.7-3-6-3zm18 0c-3.3 0-6 1.3-6 3v40c0 1.7 2.7 3 6 3s6-1.3 6-3V4c0-1.7-2.7-3-6-3z"></path></svg>`;
+		startAutoPlay();
+  } else {
+    storyPlayButton.innerHTML = `<svg aria-label="재생" class="x1lliihq x1n2onr6 xq3z1fi" fill="currentColor" height="16" role="img" viewBox="0 0 24 24" width="16"><title>재생</title><path d="M5.888 22.5a3.46 3.46 0 0 1-1.721-.46l-.003-.002a3.451 3.451 0 0 1-1.72-2.982V4.943a3.445 3.445 0 0 1 5.163-2.987l12.226 7.059a3.444 3.444 0 0 1-.001 5.967l-12.22 7.056a3.462 3.462 0 0 1-1.724.462Z"></path></svg>`;
+		clearTimeout(timer);
+    clearInterval(progressInterval);
   }
 }
 
@@ -325,6 +328,7 @@ function updateStories(userIndex, mediaIndex) {
 
 	createLoadingBars();
   updateLoadingBar(mediaIndex);
+	if (isPlaying) startAutoPlay();
 }
 
 // 재생 시간 바 생성
@@ -360,5 +364,28 @@ function updateLoadingBar(mediaIndex) {
       bar.classList.remove("active");
     }
   });
+}
+
+// 자동 재생 구현
+let timer;            
+let progressInterval;
+const STORY_DURATION = 4000;
+
+function startAutoPlay() {
+  if (!isPlaying) return;
+
+  clearTimeout(timer);
+  clearInterval(progressInterval); 
+
+  // 현재 바 애니메이션 시작
+  const currentBar = document.querySelectorAll(".story-loading-bar")[currentMediaIndex];
+  currentBar.style.transition = `width ${STORY_DURATION}ms linear`;
+  currentBar.style.width = "100%";
+
+  // 사진 전환 설정
+  timer = setTimeout(() => {
+    moveToNextStory();
+    startAutoPlay(); 
+  }, STORY_DURATION);
 }
 
