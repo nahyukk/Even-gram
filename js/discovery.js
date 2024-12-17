006a687df7627639df1c5ec2de37833cb1a355b3
seongmin <min@seongminui-MacBookPro.local>
2024-12-17 19:38:25 +0900

    랜덤 요소 랜더링

--- js/discovery.js
+++ js/discovery.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 	renderHTML("right-side-bar", "./components/sidebar.html");
 	renderHTML("footer-container", "./components/footer.html");
+	const randomImage = generateRandomImageData(16);
+	renderPosts(randomImage);
 });
 
 function renderHTML(id, html) {
@@ -42,3 +44,75 @@ function getRandomImageURL() {
 	return `https://picsum.photos/id/${randomId}/300/300`;
 }
 
+// 전체 포스트 랜더링
+function renderPosts(posts) {
+	const postContainer = document.querySelector(".discovery__post-container");
+	posts.forEach((post) => {
+		const postElement = createPostElement(post);
+		postContainer.appendChild(postElement);
+	});
+}
+
+// 포스트 요소 랜더링
+function createPostElement(post) {
+	const postElement = document.createElement("div");
+	postElement.className = "post_item-container";
+
+	const postImage = document.createElement("img");
+	postImage.className = "post_image";
+	postImage.src = post.imageUrl;
+	postImage.alt = `discover_image`;
+
+	postElement.appendChild(postImage);
+
+	if (post.isMultiple) {
+		const multipleIcon = createMultipleImageIcon();
+		postElement.appendChild(multipleIcon);
+	}
+
+	const hoverContainer = createHoverContainer(post);
+	postElement.appendChild(hoverContainer);
+
+	return postElement;
+}
+
+// 이미지 여러개일경우 표시
+function createMultipleImageIcon() {
+	const multipleIcon = document.createElement("img");
+	multipleIcon.className = "post_image-multiple";
+	multipleIcon.src = "./assets/icons/profile_post_multiple.png";
+	multipleIcon.alt = "post_image_multiple";
+
+	return multipleIcon;
+}
+
+// 호버 아이템
+function createHoverContainer(post) {
+	const hoverContainer = document.createElement("div");
+	hoverContainer.className = "post-hover-container";
+
+	const hoverItem = document.createElement("div");
+	hoverItem.className = "post-hover-item";
+	const commentItem = createHoverItem(
+		"./assets/icons/profile_post_comment.png",
+		post.comments
+	);
+	hoverItem.appendChild(commentItem);
+
+	hoverContainer.appendChild(hoverItem);
+	return hoverContainer;
+}
+
+function createHoverItem(iconSrc, textContent) {
+	const icon = document.createElement("img");
+	icon.src = iconSrc;
+
+	const span = document.createElement("span");
+	span.textContent = textContent || 0;
+
+	const container = document.createElement("div");
+	container.appendChild(icon);
+	container.appendChild(span);
+
+	return container;
+}
