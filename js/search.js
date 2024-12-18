cdfca36a18244381c042109bc422c2d301b7d38b
nahyun <knahyuk@gmail.com>
2024-12-18 11:47:33 +0900

    리스트 x 버튼 svg로 변경

--- js/search.js
+++ js/search.js
@@ -245,7 +245,7 @@ function showRecentSearches() {
 
     const deleteBtn = document.createElement("div");
     deleteBtn.classList.add("list-delete");
-    deleteBtn.textContent = "X";
+    deleteBtn.innerHTML = ` <svg aria-label="닫기" class="x1lliihq x1n2onr6 x1roi4f4" fill="currentColor" height="16" role="img" viewBox="0 0 24 24" width="16"><title>닫기</title><polyline fill="none" points="20.643 3.357 12 12 3.353 20.647" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></polyline><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" x1="20.649" x2="3.354" y1="20.649" y2="3.354"></line></svg>`;
 
     // 삭제 버튼 클릭 이벤트
     deleteBtn.addEventListener("click", (event) => {
