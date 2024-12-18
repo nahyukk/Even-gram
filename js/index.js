dc8ea7d020858f470311de80c4dd34ff5790a8f1
seongmin <min@seongminui-MacBookPro.local>
2024-12-18 13:18:58 +0900

    홈에서 sidebar html 호출하지 않도록 주석

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sidebarContainer = document.getElementById("right-side-bar");
 
-  // sidebar.html 로드
-  fetch("./components/sidebar.html")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to load sidebar.html");
-      }
-      return response.text();
-    })
-    .then((html) => {
-      sidebarContainer.innerHTML = html;
-    })
-    .catch((error) => {
-      console.error("Error loading sidebar:", error);
-    });
+  // // sidebar.html 로드
+  // fetch("../components/sidebar.html")
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Failed to load sidebar.html");
+  //     }
+  //     return response.text();
+  //   })
+  //   .then((html) => {
+  //     sidebarContainer.innerHTML = html;
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error loading sidebar:", error);
+  //   });
 });
 
 // 피드 슬라이더 스크립트
