5cabf0d74d6b9f5d9c80d9691c2f9b9b66cd258e
seongmin <min@seongminui-MacBookPro.local>
2024-12-20 14:27:35 +0900

    미사용 코드 제거

--- js/sidebar.js
+++ js/sidebar.js
@@ -84,39 +84,12 @@ export function toggleDarkModeHandler() {
 			".logo-container img, .middle-container img, .bottom-container img, .mode-menu img, .dropdown-menu img, .min-bottom-btn img, .left-sidebar-min-header img, .search-box-icon img"
 		); //sidebar의 아이콘 변경
 		const menus = document.querySelectorAll(".dropdown-menu, .mode-menu");
-		// const body = document.body;
-		// const sidebar = document.querySelector(".left-container");
-		// const rightsidebars = document.querySelector(".right-side");
-		// const hrEle = document.querySelectorAll("#moreMenu hr, #menu-header hr");
-		// const minisT = document.querySelector(".left-sidebar-min-header");
-		// const minisB = document.querySelector(".sidebar-min-botttom-contents");
-		// const searchbar = document.querySelector(".search-tab-section");
-
-		// body.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff"; // body 적용 부분
-		// // body.style.color = isDarkMode ? "#ffffff" : "#000000";
-		// sidebar.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff"; // 사이드바 부분 적용
-		// sidebar.style.borderColor = isDarkMode ? "#383838" : "#dbdbdb"; // 사이드바 border 부분 적용
-		// rightsidebars.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff";
-		// minisT.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff"; // 최소화면일때 상단 사이드바 부분
-		// minisB.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff";
-		// searchbar.style.backgroundColor = isDarkMode ? "#121212" : "#ffffff";
 
 		// 드롭다운 메뉴들 다크모드 변경
 		menus.forEach((menu) => {
 			menu.style.backgroundColor = isDarkMode ? "#272727" : "#ffffff"; //dropdown-menu 변경
 			menu.style.color = isDarkMode ? "#ffffff" : "#000000";
 		});
-
-		// hrEle.forEach((hr) => {
-		// 	hr.style.borderColor = isDarkMode ? "#444444" : "#dbdbdb4d";
-		// 	hr.style.backgroundColor = isDarkMode ? "#444444" : "#dbdbdb4d";
-		// });
-
-		// if (isDarkMode) {
-		// 	body.classList.add("dark-mode");
-		// } else {
-		// 	body.classList.remove("dark-mode");
-		// }
 
 		// 아이콘 다크모드 변경
 		icons.forEach((icon) => {
